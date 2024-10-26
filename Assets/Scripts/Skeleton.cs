e436d23bc47afbc336203a47e89bccc747aa4962
Enes Cankıymaz <enescnkymz@gmail.com>
2024-10-27 01:05:40 +0300

    saasas

---
+++ Assets/Scripts/Skeleton.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Skeleton : enemy
+{
+    public Transform target;
+    public float gorusAlani;
+    public float menzil;
+
+    void Start()
+    {
+        
+
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
