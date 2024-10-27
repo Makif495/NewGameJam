71a428c6fbf62070f99cd98459a434d6b4e3f96c
Enes Cankıymaz <enescnkymz@gmail.com>
2024-10-27 10:28:17 +0300

    alınlo

--- Assets/Scripts/enemy.cs
+++ Assets/Scripts/enemy.cs
@@ -8,4 +8,5 @@ public class enemy : MonoBehaviour
     public string karakterinlsmi;
     public int karakterinVurusGucu;
     public float karakterinHizi;
+    [HideInInspector] public bool isHiting; 
 }
